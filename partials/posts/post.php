e6d2eaa0bfadfe26e1b9bc5b295400c007e94c50
jkolassa <jkolassa@cato.org>
2015-10-26 17:25:43 -0400

    Moved the post metadata up under the headline, after which I'll style and make it fit.

--- partials/posts/post.php
+++ partials/posts/post.php
@@ -3,14 +3,12 @@
 		<h1 class="post-title">
 			<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
 		</h1>
+		<?php get_template_part( 'partials/post-metadata'); ?>
 	</header>
 	<div class="row">
 		<section class="post-content">
 			<?php the_post_thumbnail( 'medium-thumbnail' ); ?>
 			<?php the_content( __( 'Read On&hellip;', 'frank_theme' ) ); ?>
 		</section>
-		<footer class="post-info">
-			<?php get_template_part( 'partials/post-metadata' ); ?>
-		</footer>
 	</div>
 </article>
