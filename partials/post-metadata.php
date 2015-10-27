2d13935b6cd7634df731c85bcb6ed6e04b3258fc
jkolassa <jkolassa@cato.org>
2015-10-27 11:31:55 -0400

    Dramatically reduced and simplified the post metadata section into one horizontal band.

--- partials/post-metadata.php
+++ partials/post-metadata.php
@@ -1,28 +1,11 @@
-<ul class="metadata vertical">
-	<li class="date"><time datetime="<?php the_time( 'Y-m-d' ); ?>" itemprop="datePublished"><?php the_time( get_option( 'date_format' ) ); ?></time></li>
-	<li class='author'>
-		<?php
-			echo _x( 'By', 'post_author_attribution', 'frank_theme' );
-			echo ' ';
-			the_author_link();
-		?>
-	</li>
-	<li class="categories"><?php the_category( ', ' ); ?></li>
-	<li class="tags"><?php the_tags( '', ' ' ); ?></li>
-	<li class="comments">
-		<a href="<?php comments_link(); ?>">
-			<?php
-				comments_number(
-					__( 'No Comments', 'frank_theme' ),
-					__( 'One Comment', 'frank_theme' ),
-					__( '% Comments', 'frank_theme' )
-				);
-			?>
-		</a>
-	</li> 
-	<?php
-		if ( strlen( get_the_title() ) == 0 ) :
-	?>
-		<li class="permalink"><a href="<?php the_permalink() ?>">Permalink</a></li>
-	<?php endif; ?>
-</ul>
+<p class="metadata">
+   <span class="author">
+   		<?php 
+   		    echo _x('By', 'post_author_attribution', 'Jerome_theme');
+   		    echo ' ';
+   		    the_author_link();
+   		?>
+   </span>
+   <span class="category">filed under <?php the_category(', '); ?></span>
+   <span class="date">on&nbsp;<?php the_time('n/j/Y'); ?></span>
+</p>
